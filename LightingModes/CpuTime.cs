6ed61fe61ece7dde1cdb66e0f9e887e04043edf5
Tim Spannagel <mail@tim-spannagel.de>
2021-10-11 20:10:10 +0200

    Update

--- LightingModes/CpuTime.cs
+++ LightingModes/CpuTime.cs
@@ -1,4 +1,5 @@
-﻿using LedCSharp;
+﻿using ArchieB.Util;
+using LedCSharp;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -13,66 +14,77 @@ namespace ArchieB.LightingModes
         List<PerformanceCounter> cpuCounter;
         PerformanceCounter perfCounter;
         PerformanceCounterCategory perfCounterCategory = new PerformanceCounterCategory("Processor Information");
+        List<keyboardNames> cpuKeys;
+
         bool run = false;
-        public CpuTime()
+        int sleepTimer = 0;
+        public CpuTime(int sleep, Configuration config)
         {
             cpuCounter = new List<PerformanceCounter>();
             perfCounter = new PerformanceCounter("Processor Information", "% Processor Time");
             perfCounterCategory = new PerformanceCounterCategory("Processor Information");
+            cpuKeys = config.GetCpuKeys();
+            sleepTimer = sleep;
             run = true;
         }
 
         public Task Start()
         {
-            return Task.Factory.StartNew(() => {
-                while (run) { 
-                string[] cores = perfCounterCategory.GetInstanceNames();
-                foreach (string s in cores)
+            return Task.Factory.StartNew(() =>
+            {
+
+                List<string> cores = new List<string>();
+                cores.AddRange(perfCounterCategory.GetInstanceNames());
+                cores.Remove("0,_Total"); //dunno?!
+
+                if(cores.Count != cpuKeys.Count)
                 {
-                    try
-                    {
-                        string instanceName = s.Split(',')[1];
-                        cpuCounter.Add(new PerformanceCounter("Processor", "% Processor Time", instanceName));
-                    }
-                    catch (Exception ex) { }
+                    Console.WriteLine("ERROR: Too few keys defined for cpu keys.");
                 }
 
-                var key = keyboardNames.ZERO;
-                    foreach (PerformanceCounter pc in cpuCounter)
+                //order performance counters and add "_Total" add the end
+                for (int c = 0; c < cores.Count; c++)
+                {
+                    var instanceName = c.ToString();
+                    if (c == cores.Count - 1)
                     {
-                        switch (pc.InstanceName)
-                        {
-                            case "0": key = keyboardNames.F1; break;
-                            case "1": key = keyboardNames.F2; break;
-                            case "2": key = keyboardNames.F3; break;
-                            case "3": key = keyboardNames.F4; break;
-                            case "4": key = keyboardNames.F5; break;
-                            case "5": key = keyboardNames.F6; break;
-                            case "6": key = keyboardNames.F7; break;
-                            case "7": key = keyboardNames.F8; break;
-                            case "_Total": key = keyboardNames.ESC; break;
-                        }
-                        float cpuVal = pc.NextValue();
-
-
-                        int redPercent = Convert.ToInt32(((2 * (cpuVal / 100) * 255) / 255) * 100) % 100;
-                        int greenPercent = Convert.ToInt32(((255 - (cpuVal / 100) * 255) / 255) * 100);
-
-                        //Console.WriteLine("R:" + redPercent + " G:" + greenPercent);
-                        LogitechGSDK.LogiLedSetLightingForKeyWithKeyName(key, redPercent, greenPercent, 0);
-
-                        //Console.WriteLine("CPU"+pc.InstanceName + ": " + pc.NextValue() + "%");
-
-                        System.Threading.Thread.Sleep(200);
+                        instanceName = "_Total";
                     }
+                    cpuCounter.Add(new PerformanceCounter("Processor", "% Processor Time", instanceName));
+                }
+
+                while (run)
+                {
+                    int keyIterator = 0; //iterator for cpuKeys
+                    List<CpuPrintTemplate> cpuTemplates = new List<CpuPrintTemplate>();
+
+                    foreach(PerformanceCounter counter in cpuCounter)
+                    {
+                        var usage = counter.NextValue();
+                        cpuTemplates.Add(new CpuPrintTemplate(usage, counter.InstanceName));
+                        var key = cpuKeys[keyIterator];
+                        CalculateAndSetColor(key, usage);
+                        
+                        keyIterator++;
+                    }
+                    ConsolePrinter.Instance.PrintCpuUsage(cpuTemplates);
+                    System.Threading.Thread.Sleep(sleepTimer);
                 }
             });
-           
+
         }
 
         public void Stop()
         {
             run = false;
+        }
+
+        private void CalculateAndSetColor(keyboardNames key, float cpuTime)
+        {
+            int redPercent = Convert.ToInt32(((2 * (cpuTime / 100) * 255) / 255) * 100) % 100;
+            int greenPercent = Convert.ToInt32(((255 - (cpuTime / 100) * 255) / 255) * 100);
+
+            LogitechGSDK.LogiLedSetLightingForKeyWithKeyName(key, redPercent, greenPercent, 0);
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,11 +1,10 @@
-﻿ using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using ArchieB.LightingModes;
+using ArchieB.Util;
 using LedCSharp;
+using Newtonsoft.Json;
 
 namespace ArchieB
 {
@@ -13,18 +12,30 @@ namespace ArchieB
     {
         static void Main(string[] args)
         {
-            bool keepRunning = true;
-
-            LogitechGSDK.LogiLedInit();
-
-            CpuTime cpuTime = new CpuTime();
-
-            cpuTime.Start();
-            while(keepRunning)
+            string configFilePath = Environment.CurrentDirectory + "\\archieB.conf";
             {
-                
-            }
+                Console.WindowHeight = 40;
+                Console.CursorVisible = false;
+                string json = File.ReadAllText(configFilePath);
+                ConfigurationReader cr = JsonConvert.DeserializeObject<ConfigurationReader>(json);
+                var config = cr.CreateConfiguration();
 
+                bool keepRunning = true;
+
+                LogitechGSDK.LogiLedInit();
+
+                CpuTime cpuTime = new CpuTime(300, config);
+                MemUsage memUsage = new MemUsage(1000, config);
+
+                memUsage.Start();
+                cpuTime.Start();
+
+                while (keepRunning)
+                {
+                    Console.ReadKey();
+                    return;
+                }
+            }
         }
     }
 }
